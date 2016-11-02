46af22ab24f6dcc747a15cae60d9dae0b75df9fa
erikmagic <erikolivierdr@gmail.com>
2016-11-02 12:02:19 -0400

    add Main and Navigation

---
+++ Elevat8/src/primary/Main.java
@@ -0,0 +1,43 @@
+package primary;
+
+
+/**
+ * @author Erik-Olivier Riendeau, 2016
+ * Main Class that contains all parameters passed to other Classes and Threads. Also starts the ODOMETER, THE DISPLAY
+ * ,THE TIMER, THE USSENSORS AND THE COLORSENSORS. Starts by initializing the WIFI class to fetch important information
+ * as parameters for the demo.
+ *  
+ */
+public class Main {
+
+	// --------------------------------------- fields --------------------------------- //
+	
+	/**
+	 * @param args 
+	 * Main method that executs directly in the EV3
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	/**
+	 * Creates a WIFI object and accesses the object to fetch needed information
+	 */
+	public static void getWIFI(){
+		
+	}
+	
+	/**
+	 * Starts the TIMER, ODOMETER, USSENSORS, COLORSENSORS AND DISPLAY
+	 */
+	public static void startThreads(){
+		
+	}
+	/**
+	 * Initializes intances of localization and navigation
+	 */
+	public static void initializeObjects(){
+		
+	}
+}	
---
+++ Elevat8/src/primary/Navigation.java
@@ -0,0 +1,51 @@
+package primary;
+
+/**
+ * @author Erik-Olivier Riendeau, 2016
+ * Navigation class that contains useful methods for controlling the motors attached to the wheels.
+ * 
+ */
+public class Navigation {
+	
+	// ----------------------- fields ---------------------------- //
+	
+	
+	// ----------------------- constructor ----------------------- //
+	/**
+	 * Constructor called in the Main to initialize the object using the parameters in the Main
+	 */
+	public Navigation(){
+		
+	}
+	
+	/**
+	 * Simply goes forwards 
+	 */
+	public void goForwards(){
+		
+	}
+	/**
+	 * Simply goes backwards
+	 */
+	public void goBackwards(){
+		
+	}
+	/**
+	 * Travel to desired location , updating the robot's heading as he goes along. 
+	 */
+	public void travelTo(){
+		
+	}
+	/**
+	 * Rotates the robot to a desired angle
+	 */
+	public void rotateTo(){
+		
+	}
+	/**
+	 * Rotates the robot by a desired angle
+	 */
+	public void rotateBy(){
+		
+	}
+}
